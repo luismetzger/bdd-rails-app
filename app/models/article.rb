1406ae9e3e4a422b6ddec2ebf4a3f9abd73022c7
Luis Metzger <luismetzger@me.com>
2017-01-26 21:48:19 -0700

    Learned how to do flash mesages in this exercise.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+    validates_presence_of :title, :message => "Article title cannot be blank"
+    validates_presence_of :body, :message => "Article body cannot be blank"
 end
